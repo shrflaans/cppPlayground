5f0ac8b6dccb5670aac213eb5c21cfa748283892
Park <joonkyun@amazon.com>
2018-01-02 23:16:04 -0800

    First commit, dummy files

---
+++ include/Dummy.h
@@ -0,0 +1,9 @@
+class Dummy {
+
+public:
+	int getData();
+	void setData(int data);
+
+private:
+	int m_data;
+};
---
+++ src/Dummy.cpp
@@ -0,0 +1,9 @@
+#include "Dummy.h"
+
+int Dummy::getData() {
+	return m_data;
+}
+
+void Dummy::setData(int data) {
+	m_data = data;
+}
